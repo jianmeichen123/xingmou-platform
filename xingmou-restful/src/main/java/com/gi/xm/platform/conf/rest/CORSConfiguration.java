622943b0ebcb28c4533acdaeaf863678804dedd0
zhaoying <yingzhao@galaxyunternet.com>
2016-09-13 10:27:02 +0800

    --zhaoying--add cqrs

---
+++ xingmou-restful/src/main/java/com/gi/xm/platform/conf/rest/CORSConfiguration.java
@@ -0,0 +1,28 @@
+package com.gi.xm.platform.conf.rest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by zhaoying on 2016/9/13.
+ * 匹配了所有的URL，允许所有的外域发起跨域请求，允许外域发起请求任意HTTP Method，
+ * 允许跨域请求包含任意的头信息
+ */
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}

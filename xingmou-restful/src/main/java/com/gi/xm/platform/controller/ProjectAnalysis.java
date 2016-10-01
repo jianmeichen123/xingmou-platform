4538c8412eb78a746a274a9212efa09c2004521b
vincent <leizhang@galaxyinternet.com>
2016-10-01 20:54:46 +0800

    dev

--- xingmou-restful/src/main/java/com/gi/xm/platform/controller/ProjectAnalysis.java
+++ xingmou-restful/src/main/java/com/gi/xm/platform/controller/ProjectAnalysis.java
@@ -1,13 +1,11 @@
 package com.gi.xm.platform.controller;
 
-import com.alibaba.druid.sql.visitor.functions.Char;
-import com.gi.xm.platform.view.LabelInfo;
-import com.gi.xm.platform.view.common.Contants;
 import com.gi.xm.platform.view.common.MessageInfo;
-import com.gi.xm.report.biz.ChartEventIndustryDistictYearBiz;
+import com.gi.xm.report.biz.ChartProjectIndustrySubYearBiz;
+import com.gi.xm.report.biz.ChartProjectIndustryYearBiz;
 import com.gi.xm.report.common.ChartPojo;
-import com.gi.xm.report.common.Message;
-import com.gi.xm.report.pojo.ChartEventIndustryDistictYear;
+import com.gi.xm.report.pojo.ChartProjectIndustrySubYear;
+import com.gi.xm.report.pojo.ChartProjectIndustryYear;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +23,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("projectAnalysis")
 public class ProjectAnalysis {
-
+/*
     @Autowired
     private ChartEventIndustryDistictYearBiz chartEventIndustryDistictYearBiz;
 
@@ -60,5 +58,85 @@ public class ProjectAnalysis {
         chartPojo.setRaw(data);
         messageInfo.setData(chartPojo);
         return messageInfo;
+    }*/
+
+
+    @Autowired
+    private ChartProjectIndustryYearBiz chartProjectIndustryYearBiz;
+
+
+    @Autowired
+    private ChartProjectIndustrySubYearBiz chartProjectIndustrySubYearBiz;
+
+
+    @RequestMapping("bar/{years}/{industryNames}/{industryId}")
+    @ResponseBody
+    public MessageInfo bar(@PathVariable("years") Integer[] years,@PathVariable("industryNames") String[] industryNames,@PathVariable("industryId") Integer industryId) {
+
+        if (industryNames != null && industryId == 0) {
+            MessageInfo<List<ChartProjectIndustryYear>> messageInfo = chartProjectIndustryYearBiz.selectByYear(years);
+            if (!messageInfo.isSuccess()) {
+                return messageInfo;
+            }
+            Map<String, Map<Integer, Map<String, Long>>> data = initMap(years, industryNames);
+            for (ChartProjectIndustryYear c : messageInfo.getData()) {
+                if (c.getYear() != null && c.getIndustryName() != null) {
+                    data.get("projectNums").get(c.getYear()).put(c.getIndustryName(), c.getProjectNum().longValue());
+                    data.get("projectInvestedNums").get(c.getYear()).put(c.getIndustryName(), c.getProjectInvestedNum().longValue());
+                    data.get("projectunInvestedNums").get(c.getYear()).put(c.getIndustryName(), c.getProjectUninvestedNum().longValue());
+                }
+            }
+            MessageInfo<ChartPojo> m = new MessageInfo<>();
+            ChartPojo c = new ChartPojo();
+            c.setRaw(data);
+            m.setData(c);
+            return m;
+        } else if (industryNames != null && industryId > 0) {
+            MessageInfo<List<ChartProjectIndustrySubYear>> messageInfo = chartProjectIndustrySubYearBiz.getListByYear(years, industryId);
+            if (!messageInfo.isSuccess()) {
+                return messageInfo;
+            }
+            Map<String, Map<Integer, Map<String, Long>>> data = initMap(years, industryNames);
+            for (ChartProjectIndustrySubYear c : messageInfo.getData()) {
+                if (c.getYear() != null && c.getIndustrySubName() != null&&data.get("projectNums").get(c.getYear())!=null) {
+                    data.get("projectNums").get(c.getYear()).put(c.getIndustrySubName(), c.getProjectNum().longValue());
+                    data.get("projectInvestedNums").get(c.getYear()).put(c.getIndustrySubName(), c.getProjectInvestedNum().longValue());
+                    data.get("projectunInvestedNums").get(c.getYear()).put(c.getIndustrySubName(), c.getProjectUninvestedNum().longValue());
+                }
+            }
+            MessageInfo<ChartPojo> m = new MessageInfo<>();
+            ChartPojo c = new ChartPojo();
+            c.setRaw(data);
+            m.setData(c);
+            return m;
+        } else {
+            return null;
+        }
     }
+
+
+    private Map<String,Map<Integer,Map<String,Long>>> initMap(Integer[] years, String[] industryNames){
+        Map<Integer,Map<String,Long>> projectNums = new HashMap<>();
+        Map<Integer,Map<String,Long>> projectInvestedNums = new HashMap<>();
+        Map<Integer,Map<String,Long>> projectunInvestedNums = new HashMap<>();
+        for(Integer year:years){
+            Map<String,Long> pns = new HashMap<>();
+            projectNums.put(year,pns);
+            Map<String,Long> pins = new HashMap<>();
+            projectInvestedNums.put(year,pins);
+            Map<String,Long> puins = new HashMap<>();
+            projectunInvestedNums.put(year,puins);
+            for(String industryName :industryNames){
+                pns.put(industryName,0l);
+                pins.put(industryName,0l);
+                puins.put(industryName,0l);
+            }
+        }
+        Map<String,Map<Integer,Map<String,Long>>> data =new HashMap<>();
+        data.put("projectNums",projectNums);
+        data.put("projectInvestedNums",projectInvestedNums);
+        data.put("projectunInvestedNums",projectunInvestedNums);
+        return data;
+    }
+
 }

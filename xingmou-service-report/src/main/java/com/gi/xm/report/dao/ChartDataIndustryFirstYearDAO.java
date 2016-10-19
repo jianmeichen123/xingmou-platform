cb8116400f64add90b28e31df9fcc489e0e6c9e2
vincent <leizhang@galaxyinternet.com>
2016-10-19 09:39:36 +0800

    dubbo-->biz

--- xingmou-service-report/src/main/java/com/gi/xm/report/dao/ChartDataIndustryFirstYearDAO.java
+++ xingmou-service-report/src/main/java/com/gi/xm/report/dao/ChartDataIndustryFirstYearDAO.java
@@ -3,6 +3,8 @@ package com.gi.xm.report.dao;
 import java.util.Date;
 import java.math.BigDecimal;
 import java.util.List;
+
+import com.gi.xm.report.query.ChartDataIndustryFirstYearQueryInfo;
 import org.apache.ibatis.annotations.Param;
 
 import com.gi.xm.report.pojo.ChartDataIndustryFirstYear;
@@ -18,6 +20,6 @@ public interface ChartDataIndustryFirstYearDAO {
 	/*List<ChartDataIndustryFirstYear> selectByYaar(@Param("years") Integer[] years);
 				
 	List<ChartDataIndustryFirstYear> selectByIndustryId(@Param("industryId") Integer industryId);*/
-				
 
+    List<ChartDataIndustryFirstYear> queryChartDataIndustryFirstYear(ChartDataIndustryFirstYearQueryInfo chartDataIndustryFirstYearQueryInfo);
 }
--- xingmou-service-report/src/main/java/com/gi/xm/report/dao/ChartDataIndustryYearDAO.java
+++ xingmou-service-report/src/main/java/com/gi/xm/report/dao/ChartDataIndustryYearDAO.java
@@ -3,6 +3,8 @@ package com.gi.xm.report.dao;
 import java.util.Date;
 import java.math.BigDecimal;
 import java.util.List;
+
+import com.gi.xm.report.query.ChartDataIndustryYearQueryInfo;
 import org.apache.ibatis.annotations.Param;
 
 import com.gi.xm.report.pojo.ChartDataIndustryYear;
@@ -20,5 +22,7 @@ public interface ChartDataIndustryYearDAO {
 /*	List<ChartDataIndustryYear> selectByIndustryId(@Param("industryId") Integer industryId);
 				
 	List<ChartDataIndustryYear> selectByYaar(@Param("yaar") Integer yaar);*/
-		
+
+
+	List<ChartDataIndustryYear> queryChartDataIndustryYear(ChartDataIndustryYearQueryInfo chartDataIndustryYearQueryInfo);
 }

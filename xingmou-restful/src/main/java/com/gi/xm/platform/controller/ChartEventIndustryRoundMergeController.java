c893f712fa73da589ab3b8fa0930bfa9306ff3e8
zhaoying <yingzhao@galaxyunternet.com>
2016-10-25 16:52:41 +0800

    --zhaoying--update

--- xingmou-restful/src/main/java/com/gi/xm/platform/controller/ChartEventIndustryRoundMergeController.java
+++ xingmou-restful/src/main/java/com/gi/xm/platform/controller/ChartEventIndustryRoundMergeController.java
@@ -8,10 +8,13 @@ import com.alibaba.dubbo.config.annotation.Reference;
 
 import com.gi.xm.platform.view.common.MessageInfo;
 import com.gi.xm.platform.view.common.QueryResultInfo;
+import com.gi.xm.report.biz.ChartDataIndustryFirstYearBiz;
+import com.gi.xm.report.biz.ChartDataIndustryYearBiz;
 import com.gi.xm.report.biz.ChartEventIndustryRoundMergeBiz;
-import com.gi.xm.report.pojo.ChartEventIndustryRoundMerge;
+import com.gi.xm.report.pojo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +26,11 @@ public class ChartEventIndustryRoundMergeController {
 
 	@Autowired
 	private ChartEventIndustryRoundMergeBiz chartEventIndustryRoundMergeBiz;
+	@Autowired
+	private ChartDataIndustryFirstYearBiz chartDataIndustryFirstYearBiz;
+
+	@Autowired
+	private ChartDataIndustryYearBiz chartDataIndustryYearBiz;
 
 	@RequestMapping("search")
 	@ResponseBody
@@ -38,6 +46,22 @@ public class ChartEventIndustryRoundMergeController {
 		MessageInfo<List<ChartEventIndustryRoundMerge>> resultMessageInfo = chartEventIndustryRoundMergeBiz.chartAnalysis(chartEventIndustryRoundMerge);
 		return resultMessageInfo;
 	}
+	@RequestMapping("chartDataYear/{years}/{industryId}")
+	@ResponseBody
+	public MessageInfo chartDataYear(@PathVariable("years") Integer[] years, @PathVariable("industryId") Integer industryId){
 
+		//所有一级行业
+		if (industryId==null) {
+
+		} else {
+			//二级行业
+		}
+
+
+		return null;
+
+
+
+	}
 	
 }

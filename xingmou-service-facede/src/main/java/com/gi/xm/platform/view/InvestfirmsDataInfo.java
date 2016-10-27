9738915ad1cb3cf6a2518ab602a6a387982fa210
vincent <leizhang@galaxyinternet.com>
2016-10-27 16:36:52 +0800

    sourceId

--- xingmou-service-facede/src/main/java/com/gi/xm/platform/view/InvestfirmsDataInfo.java
+++ xingmou-service-facede/src/main/java/com/gi/xm/platform/view/InvestfirmsDataInfo.java
@@ -7,69 +7,69 @@ import java.util.Date;
 
 public class InvestfirmsDataInfo implements Serializable {
 
-	/**   
-	 * @Fields serialVersionUID : �??????????? 
-	 */ 
+	/**
+	 * @Fields serialVersionUID : �???????????
+	 */
 	private static final long serialVersionUID = 1L;
 
-			
+
 	/**
-	 * @Fields id : 
+	 * @Fields id :
 	 */
 	private Integer id;
-			
+
 	/**
 	 * @Fields investfirmId : ??�??��??id
 	 */
 	private Long investfirmId;
-			
+
 	/**
 	 * @Fields investfirmName : ??�???�??��????�?
 	 */
 	private String investfirmName;
-			
+
 	/**
 	 * @Fields investfirmLink : ??�??��??�???
 	 */
 	private String investfirmLink;
-			
+
 	/**
 	 * @Fields investNum : ?��??�?�???
 	 */
 	private Integer investNum;
-			
+
 	/**
 	 * @Fields investMoney : ?��??�???�?�???�?
 	 */
 	private Long investMoney;
-			
+
 	/**
 	 * @Fields investIndustry : ??�?�???�?级�?�?
 	 */
 	private String investIndustry;
-			
+
 	/**
 	 * @Fields investRound : ??�?�???�?�?
 	 */
 	private String investRound;
-			
+
 	/**
 	 * @Fields newestInvestProjects : ??�?�???项�????称�???�?�?�?�?
 	 */
 	private String newestInvestProjects;
-			
+
 	/**
 	 * @Fields investMonthRate : �?????�?�??��??�?�??��?
 	 */
 	private Integer investMonthRate;
-			
+
 	/**
-	 * @Fields addTime : 
+	 * @Fields addTime :
 	 */
 	private Timestamp addTime;
-			
+
 	/**
-	 * @Fields updateTime : 
+	 * @Fields updateTime :
 	 */
 	private Timestamp updateTime;
 
@@ -78,16 +78,6 @@ public class InvestfirmsDataInfo implements Serializable {
 	private Long[] industryIds;
 
 	private String pic;
-
-	private  Long sourceId;
-
-	public Long getSourceId() {
-		return sourceId;
-	}
-
-	public void setSourceId(Long sourceId) {
-		this.sourceId = sourceId;
-	}
 
 	public String getPic() {
 		return pic;
@@ -116,95 +106,95 @@ public class InvestfirmsDataInfo implements Serializable {
 	public Integer getId(){
 		return id;
 	}
-	
+
 	public void setId(Integer id){
 		this.id = id;
 	}
-    
+
 	public Long getInvestfirmId(){
 		return investfirmId;
 	}
-	
+
 	public void setInvestfirmId(Long investfirmId){
 		this.investfirmId = investfirmId;
 	}
-    
+
 	public String getInvestfirmName(){
 		return investfirmName;
 	}
-	
+
 	public void setInvestfirmName(String investfirmName){
 		this.investfirmName = investfirmName;
 	}
-    
+
 	public String getInvestfirmLink(){
 		return investfirmLink;
 	}
-	
+
 	public void setInvestfirmLink(String investfirmLink){
 		this.investfirmLink = investfirmLink;
 	}
-    
+
 	public Integer getInvestNum(){
 		return investNum;
 	}
-	
+
 	public void setInvestNum(Integer investNum){
 		this.investNum = investNum;
 	}
-    
+
 	public Long getInvestMoney(){
 		return investMoney;
 	}
-	
+
 	public void setInvestMoney(Long investMoney){
 		this.investMoney = investMoney;
 	}
-    
+
 	public String getInvestIndustry(){
 		return investIndustry;
 	}
-	
+
 	public void setInvestIndustry(String investIndustry){
 		this.investIndustry = investIndustry;
 	}
-    
+
 	public String getInvestRound(){
 		return investRound;
 	}
-	
+
 	public void setInvestRound(String investRound){
 		this.investRound = investRound;
 	}
-    
+
 	public String getNewestInvestProjects(){
 		return newestInvestProjects;
 	}
-	
+
 	public void setNewestInvestProjects(String newestInvestProjects){
 		this.newestInvestProjects = newestInvestProjects;
 	}
-    
+
 	public Integer getInvestMonthRate(){
 		return investMonthRate;
 	}
-	
+
 	public void setInvestMonthRate(Integer investMonthRate){
 		this.investMonthRate = investMonthRate;
 	}
-    
+
 	public Timestamp getAddTime(){
 		return addTime;
 	}
-	
+
 	public void setAddTime(Timestamp addTime){
 		this.addTime = addTime;
 	}
-    
+
 	public Timestamp getUpdateTime(){
 		return updateTime;
 	}
-	
+
 	public void setUpdateTime(Timestamp updateTime){
 		this.updateTime = updateTime;
 	}

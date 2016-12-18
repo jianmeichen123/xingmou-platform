ada6331c9ef948d6377bfb2c6f59b401ab244b45
vincent <leizhang@galaxyinternet.com>
2016-12-18 12:42:22 +0800

    不跟踪

--- bin/single-dubbo-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mvn clean install -f pom-sd.xml
-mvn clean install -f xingmou-assembly/pom-sd.xml
-mvn clean install -f xingmou-dal/pom-sd.xml
-mvn clean install -f xingmou-biz/pom-sd.xml
-mvn clean install -f xingmou-service-facede/pom.xml
-mvn clean install -f xingmou-service/pom-sd.xml
-

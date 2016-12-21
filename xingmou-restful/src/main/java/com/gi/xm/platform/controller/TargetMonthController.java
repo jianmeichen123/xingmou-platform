31c7b5e6cfc7c944f9c7fcb631c0bf9b5244e005
vincent <leizhang@galaxyinternet.com>
2016-12-21 16:09:19 +0800

    Autowired

--- xingmou-restful/src/main/java/com/gi/xm/platform/controller/TargetMonthController.java
+++ xingmou-restful/src/main/java/com/gi/xm/platform/controller/TargetMonthController.java
@@ -1,12 +1,11 @@
 package com.gi.xm.platform.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import com.gi.xm.platform.view.common.MessageInfo;
 import com.gi.xm.platform.view.common.QueryResultInfo;
 import com.gi.xm.report.biz.TargetMonthBiz;
-import com.gi.xm.report.common.Message;
 import com.gi.xm.report.pojo.TargetMonth;
 import com.gi.xm.report.query.TargetMonthQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,21 +16,21 @@ import java.util.List;
 @RequestMapping("targetMonth")
 public class TargetMonthController {
 
-    @Reference
-	private TargetMonthBiz targetMonthBiz;
+    @Autowired
+    private TargetMonthBiz targetMonthBiz;
 
-	@RequestMapping("query")
-	@ResponseBody
-	public MessageInfo<QueryResultInfo<TargetMonth>> queryTargetMonth (TargetMonthQuery targetMonthQueryInfo) {
-		MessageInfo<QueryResultInfo<TargetMonth>> resultMessageInfo = targetMonthBiz.queryTargetMonth(targetMonthQueryInfo);
-		return resultMessageInfo;
-	}
+    @RequestMapping("query")
+    @ResponseBody
+    public MessageInfo<QueryResultInfo<TargetMonth>> queryTargetMonth (TargetMonthQuery targetMonthQueryInfo) {
+        MessageInfo<QueryResultInfo<TargetMonth>> resultMessageInfo = targetMonthBiz.queryTargetMonth(targetMonthQueryInfo);
+        return resultMessageInfo;
+    }
 
-	@RequestMapping("queryList")
-	@ResponseBody
-	public MessageInfo<List<TargetMonth>>  queryTargetMonthList (TargetMonthQuery targetMonthQueryInfo) {
-		MessageInfo<List<TargetMonth>> resultMessageInfo = targetMonthBiz.queryTargetMonthList(targetMonthQueryInfo);
-		return resultMessageInfo;
-	}
+    @RequestMapping("queryList")
+    @ResponseBody
+    public MessageInfo<List<TargetMonth>>  queryTargetMonthList (TargetMonthQuery targetMonthQueryInfo) {
+        MessageInfo<List<TargetMonth>> resultMessageInfo = targetMonthBiz.queryTargetMonthList(targetMonthQueryInfo);
+        return resultMessageInfo;
+    }
 
 }

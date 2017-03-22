ad5ea199d53076bd01ac45881539af064d1fba4f
vincent <q>
2017-03-22 13:28:52 +0800

    op

--- ctdn-data-ods/src/main/java/com/gi/ctdn/ods/pojo/OpIndiceSummary.java
+++ ctdn-data-ods/src/main/java/com/gi/ctdn/ods/pojo/OpIndiceSummary.java
@@ -6,19 +6,17 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.gi.xm.platform.view.common.PojoInfo;
 
 public class OpIndiceSummary extends PojoInfo {
 
         public static final String ID = "id";
-        public static final String DATE = "指标计算时间";
-        public static final String TITLE = "项目title";
         public static final String CODE = "项目code";
         public static final String APPNAME = "网站域名,appName,微博、微信名称";
         public static final String TYPE = "domain,ios,android,weibo,weixin";
-        public static final String INDEX_1 = "指标1：PV/UV、APP的下载总量、微博/微信的平均阅读量";
-        public static final String INDEX_2 = "指标2：排名、APP当天的下载量、微博的粉丝数、微信的点赞量";
+        public static final String APPID = "appid";
+        public static final String INDEX_1 = "index1";
+        public static final String INDEX_2 = "index2";
         public static final String AVG_SCORE = "APP评分";
         public static final String UPDATE_DATE = "指标获取时间";
     
@@ -30,17 +28,6 @@ public class OpIndiceSummary extends PojoInfo {
 	private Integer id;
 	
 	/**
-	 * @Fields date : 指标计算时间
-	 */
-	private Integer date;
-	
-	/**
-	 * @Fields title : 项目title
-	 */
-	@JsonIgnore
-	private String title;
-	
-	/**
 	 * @Fields code : 项目code
 	 */
 	@JsonIgnore
@@ -49,7 +36,6 @@ public class OpIndiceSummary extends PojoInfo {
 	/**
 	 * @Fields appname : 网站域名,appName,微博、微信名称
 	 */
-	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String appname;
 	
 	/**
@@ -58,27 +44,28 @@ public class OpIndiceSummary extends PojoInfo {
 	private String type;
 	
 	/**
-	 * @Fields index1 : 指标1：PV/UV、APP的下载总量、微博/微信的平均阅读量
+	 * @Fields appid : 
 	 */
-	@JsonInclude(JsonInclude.Include.NON_NULL)
-	private Integer index1;
+	private String appid;
 	
 	/**
-	 * @Fields index2 : 指标2：排名、APP当天的下载量、微博的粉丝数、微信的点赞量
+	 * @Fields index1 : 
 	 */
-	@JsonInclude(JsonInclude.Include.NON_NULL)
-	private Integer index2;
+	private Long index1;
+	
+	/**
+	 * @Fields index2 : 
+	 */
+	private Long index2;
 	
 	/**
 	 * @Fields avgScore : APP评分
 	 */
-	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private Integer avgScore;
 	
 	/**
 	 * @Fields updateDate : 指标获取时间
 	 */
-	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private Integer updateDate;
 	
     
@@ -88,22 +75,6 @@ public class OpIndiceSummary extends PojoInfo {
 
 	public void setId(Integer id){
 		this.id = id;
-	}
-    
-	public Integer getDate(){
-		return date;
-	}
-
-	public void setDate(Integer date){
-		this.date = date;
-	}
-    
-	public String getTitle(){
-		return title;
-	}
-
-	public void setTitle(String title){
-		this.title = title;
 	}
     
 	public String getCode(){
@@ -130,19 +101,27 @@ public class OpIndiceSummary extends PojoInfo {
 		this.type = type;
 	}
     
-	public Integer getIndex1(){
+	public String getAppid(){
+		return appid;
+	}
+
+	public void setAppid(String appid){
+		this.appid = appid;
+	}
+    
+	public Long getIndex1(){
 		return index1;
 	}
 
-	public void setIndex1(Integer index1){
+	public void setIndex1(Long index1){
 		this.index1 = index1;
 	}
     
-	public Integer getIndex2(){
+	public Long getIndex2(){
 		return index2;
 	}
 
-	public void setIndex2(Integer index2){
+	public void setIndex2(Long index2){
 		this.index2 = index2;
 	}
     

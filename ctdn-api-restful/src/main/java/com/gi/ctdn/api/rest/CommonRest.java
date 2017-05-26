357340e70efc2e319a57306ec5444a12d79323a2
vincent <leizhang>
2017-05-26 12:15:27 +0800

    alquery

--- ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/CommonRest.java
+++ ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/CommonRest.java
@@ -505,6 +505,7 @@ public class CommonRest {
      */
     @RequestMapping("allQuery")
     @ResponseBody
+    @Cacheable(value = "allQuery",keyGenerator = "baseKG")
     public MessageInfo<Map> allQuery(){
         //企业
         MessageInfo<Map> messageInfo = new MessageInfo<>();

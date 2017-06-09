f1f1ff25458d373d62748591b14149b46ee8fb9a
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-09 11:02:58 +0800

    rest

---
+++ ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/EventDetailController.java
@@ -0,0 +1,50 @@
+package com.gi.ctdn.api.rest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import com.gi.ctdn.biz.EventDetailBiz;
+import com.gi.ctdn.pojo.EventDetail;
+import com.gi.xm.platform.view.common.MessageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("eventDetail")
+public class EventDetailController {
+
+    @Autowired
+	private EventDetailBiz eventDetailBiz;
+
+
+
+    @RequestMapping("getAll")
+    @ResponseBody
+    public MessageInfo<List<EventDetail>> getAllEventDetail(){
+		MessageInfo<List<EventDetail>>  messageInfo = eventDetailBiz.getAllEventDetail();
+		return messageInfo;
+	}
+
+
+		
+    @RequestMapping("getListByEventId")
+    @ResponseBody
+	public MessageInfo<List<EventDetail>> getListByEventId(Integer eventId){
+		MessageInfo<List<EventDetail>> messageInfo = eventDetailBiz.getListByEventId(eventId);
+		return messageInfo;
+	}
+
+	@RequestMapping("getListByProjTitle")
+	@ResponseBody
+	public MessageInfo<List<EventDetail>> getListByProjTitle(String projTitle){
+		MessageInfo<List<EventDetail>> messageInfo = eventDetailBiz.getListByProjTitle(projTitle);
+		return messageInfo;
+	}
+}

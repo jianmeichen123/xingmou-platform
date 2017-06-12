50a9a6012cbddb0eef295d0adf718ae55b78d975
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-12 15:36:22 +0800

    rest

--- ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/OrgInfoController.java
+++ ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/OrgInfoController.java
@@ -3,10 +3,15 @@ package com.gi.ctdn.api.rest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 
 import com.gi.ctdn.biz.OrgInfoBiz;
+import com.gi.ctdn.biz.OrgMediaInfoBiz;
+import com.gi.ctdn.biz.OrgMemberInfoBiz;
 import com.gi.ctdn.pojo.OrgInfo;
+import com.gi.ctdn.pojo.OrgMediaInfo;
+import com.gi.ctdn.pojo.OrgMemberInfo;
 import com.gi.xm.platform.view.common.MessageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,6 +29,12 @@ public class OrgInfoController {
     @Autowired
 	private OrgInfoBiz orgInfoBiz;
 
+    @Autowired
+	private OrgMediaInfoBiz orgMediaInfoBiz;
+
+    @Autowired
+	private OrgMemberInfoBiz orgMemberInfoBiz;
+
 
     @RequestMapping("getAll")
     @ResponseBody
@@ -36,8 +47,26 @@ public class OrgInfoController {
 		
     @RequestMapping("getListByOrgId/{orgId}")
     @ResponseBody
-	public MessageInfo<List<OrgInfo>> getListByOrgId(@PathVariable Integer orgId){
-		MessageInfo<List<OrgInfo>> messageInfo = orgInfoBiz.getListByOrgId(orgId);
+	public MessageInfo<Map<String,Object>> getListByOrgId(@PathVariable Integer orgId){
+    	MessageInfo<Map<String,Object>> messageInfo = new MessageInfo<>();
+    	Map<String,Object> map = new HashMap<>();
+
+		MessageInfo<List<OrgInfo>> orgInfoMessageInfo = orgInfoBiz.getListByOrgId(orgId);
+		map.put("orgInfoMessageInfo",orgInfoMessageInfo);
+		orgInfoMessageInfo.setMessage(orgInfoMessageInfo.getMessage());
+		orgInfoMessageInfo.setStatus(orgInfoMessageInfo.getStatus());
+
+		MessageInfo<List<OrgMediaInfo>> orgMediaInfoMessageInfo = orgMediaInfoBiz.getListByOrgId(orgId);
+		map.put("orgMediaInfoMessageInfo",orgMediaInfoMessageInfo);
+		orgMediaInfoMessageInfo.setMessage(orgMediaInfoMessageInfo.getMessage());
+		orgMediaInfoMessageInfo.setStatus(orgMediaInfoMessageInfo.getStatus());
+
+		MessageInfo<List<OrgMemberInfo>> orgMemberInfoMessageInfo = orgMemberInfoBiz.getListByOrgId(orgId);
+		map.put("orgMemberInfoMessageInfo",orgMemberInfoMessageInfo);
+		orgMemberInfoMessageInfo.setMessage(orgMemberInfoMessageInfo.getMessage());
+		orgMemberInfoMessageInfo.setStatus(orgMemberInfoMessageInfo.getStatus());
+
+		messageInfo.setData(map);
 		return messageInfo;
 	}
 

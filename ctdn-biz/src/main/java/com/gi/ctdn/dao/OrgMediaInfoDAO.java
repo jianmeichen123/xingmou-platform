c96958c713781e4f004f444fe57ae4a776cc195c
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-12 09:26:27 +0800

    rest

---
+++ ctdn-biz/src/main/java/com/gi/ctdn/dao/OrgMediaInfoDAO.java
@@ -0,0 +1,16 @@
+package com.gi.ctdn.dao;
+
+import java.util.Date;
+import java.math.BigDecimal;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.gi.ctdn.pojo.OrgMediaInfo;
+
+public interface OrgMediaInfoDAO {
+	
+			
+	List<OrgMediaInfo> selectByOrgId(@Param("orgId") Integer orgId);
+		
+	List<OrgMediaInfo> selectAll();
+}

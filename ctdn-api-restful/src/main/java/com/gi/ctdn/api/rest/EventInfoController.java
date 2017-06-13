6de666c93f75231bcc29d9f949cf6884d9e1c21e
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-13 16:38:00 +0800

    rest

--- ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/EventInfoController.java
+++ ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/EventInfoController.java
@@ -2,19 +2,13 @@ package com.gi.ctdn.api.rest;
 
 import com.gi.ctdn.biz.EventDetailBiz;
 import com.gi.ctdn.biz.EventInfoBiz;
-import com.gi.ctdn.pojo.EventDetail;
 import com.gi.ctdn.pojo.EventInfo;
 import com.gi.xm.platform.view.common.MessageInfo;
-import com.gi.xm.platform.view.common.QueryResultInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("eventInfo")
@@ -33,21 +27,8 @@ public class EventInfoController {
 	 */
 	@RequestMapping("getById/{eventId}")
 	@ResponseBody
-	public MessageInfo<Map<String,Object>> getEventInfo(@PathVariable Integer eventId ){
-		MessageInfo<Map<String,Object>> messageInfo = new MessageInfo<>();
-		Map<String,Object> map = new HashMap<>();
-
-		MessageInfo<EventInfo> eventInfoMessageInfo =  eventInfoBiz.getByEventId(eventId);
-		map.put("eventInfoMessageInfo",eventInfoMessageInfo.getData());
-		messageInfo.setMessage(eventInfoMessageInfo.getMessage());
-		messageInfo.setStatus(eventInfoMessageInfo.getStatus());
-
-		MessageInfo<List<EventDetail>> eventDetailInfoMessageInfo = eventDetailBiz.getListByEventId(eventId);
-		map.put("eventDetailInfoMessageInfo",eventDetailInfoMessageInfo.getData());
-		messageInfo.setMessage(eventDetailInfoMessageInfo.getMessage());
-		messageInfo.setStatus(eventDetailInfoMessageInfo.getStatus());
-
-		messageInfo.setData(map);
+	public MessageInfo<EventInfo> getEventInfo(@PathVariable Integer eventId ){
+		MessageInfo<EventInfo> messageInfo = eventInfoBiz.getByEventId(eventId);
 		return messageInfo;
 	}
 

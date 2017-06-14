45e57424691d41511e957043dbd6f2560496d88c
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-14 10:56:17 +0800

    rest

--- ctdn-biz/src/main/java/com/gi/ctdn/dao/OrgInfoDAO.java
+++ ctdn-biz/src/main/java/com/gi/ctdn/dao/OrgInfoDAO.java
@@ -3,6 +3,8 @@ package com.gi.ctdn.dao;
 import java.util.Date;
 import java.math.BigDecimal;
 import java.util.List;
+
+import com.gi.ctdn.pojo.OrgListInfo;
 import org.apache.ibatis.annotations.Param;
 
 import com.gi.ctdn.pojo.OrgInfo;
@@ -13,4 +15,6 @@ public interface OrgInfoDAO {
 	List<OrgInfo> selectByOrgId(@Param("orgId") Integer orgId);
 
 	List<OrgInfo> selectAll();
+
+	OrgListInfo selectById(@Param("orgId") Integer orgId);
 }

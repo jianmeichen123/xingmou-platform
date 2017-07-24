e33c4ab279d9f93c9f030217e2e40538b4f64525
wanghaonan <haonanwang@galaxyinternet.com>
2017-07-24 18:01:16 +0800

    工商信息controller

--- xingmou-data-rest/src/main/java/com/gi/xm/platform/restful/ProjectBusinessInfoController.java
+++ xingmou-data-rest/src/main/java/com/gi/xm/platform/restful/ProjectBusinessInfoController.java
@@ -7,10 +7,12 @@ import java.util.Map;
 
 import com.gi.ctdn.ods.biz.ProjectBusinessInfoBiz;
 import com.gi.ctdn.ods.pojo.ProjectBusinessInfo;
+import com.gi.ctdn.ods.pojo.ProjectBusinessListInfo;
 import com.gi.xm.platform.mongodb.common.QueryResult;
 import com.gi.xm.platform.view.common.MessageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-@RequestMapping("projectBusinessInfo")
+@RequestMapping("businessInfo")
 public class ProjectBusinessInfoController {
 
     @Autowired
@@ -34,10 +36,10 @@ public class ProjectBusinessInfoController {
 
 
 		
-    @RequestMapping("getListBySourceCode")
+    @RequestMapping("getListBySourceCode/{sourceCode}")
     @ResponseBody
-	public MessageInfo<List<ProjectBusinessInfo>> getListBySourceCode(String sourceCode){
-		MessageInfo<List<ProjectBusinessInfo>> messageInfo = projectBusinessInfoBiz.getListBySourceCode(sourceCode);
+	public MessageInfo<ProjectBusinessListInfo> getListBySourceCode(@PathVariable String sourceCode){
+		MessageInfo<ProjectBusinessListInfo> messageInfo = projectBusinessInfoBiz.getProjectBusinessListInfoBySourceCode(sourceCode);
 		return messageInfo;
 	}
 		

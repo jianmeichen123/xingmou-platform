76b11ab6ae6b1f59effc5ab444d009d0824a99e3
wanghaonan <haonanwang@galaxyinternet.com>
2017-07-24 17:06:00 +0800

    工商信息dao

--- ctdn-data-ods/src/main/java/com/gi/ctdn/ods/dao/ProjectBusinessInfoDAO.java
+++ ctdn-data-ods/src/main/java/com/gi/ctdn/ods/dao/ProjectBusinessInfoDAO.java
@@ -5,13 +5,16 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import com.gi.ctdn.ods.pojo.ProjectBusinessInfo;
+import com.gi.ctdn.ods.pojo.ProjectBusinessListInfo;
 import org.apache.ibatis.annotations.Param;
 
 
 public interface ProjectBusinessInfoDAO {
 	
 			
+	ProjectBusinessListInfo selectProjectBusinessInfoBySourceCode(@Param("sourceCode") String sourceCode);
+
 	List<ProjectBusinessInfo> selectBySourceCode(@Param("sourceCode") String sourceCode);
-		
+
 	List<ProjectBusinessInfo> selectAll();
 }

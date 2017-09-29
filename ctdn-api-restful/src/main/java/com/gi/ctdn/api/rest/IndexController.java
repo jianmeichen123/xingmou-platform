46cfeaaf2a8b90e2a9f2334c603284175d709d0f
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-09-29 10:08:34 +0800

    dev_3.4

---
+++ ctdn-api-restful/src/main/java/com/gi/ctdn/api/rest/IndexController.java
@@ -0,0 +1,53 @@
+package com.gi.ctdn.api.rest;
+
+import com.gi.ctdn.biz.IndexHeaderStatBiz;
+import com.gi.ctdn.biz.UserIndustryBiz;
+import com.gi.ctdn.pojo.IndexHeaderStat;
+import com.gi.ctdn.pojo.UserIndustry;
+import com.gi.xm.platform.view.common.MessageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by zcy on 17-9-5.
+ */
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+//    @Autowired
+//    private UserIndustryBiz userIndustryBiz;
+
+    @Autowired
+    private IndexHeaderStatBiz indexHeaderStatBiz;
+
+//    /**
+//     *查询用户关注行业
+//     * @return messageInfo
+//     */
+//    @RequestMapping("userIndustry/{userId}")
+//    @ResponseBody
+//    public MessageInfo<UserIndustry> getUserIndustry (@PathVariable("userId") Integer userId){
+//        MessageInfo<UserIndustry> messageInfo = userIndustryBiz.getUserIndustry(userId);
+//        return messageInfo;
+//    }
+//
+//    /**
+//     *设置用户关注行业
+//     * @return messageInfo
+//     */
+//    @RequestMapping("updateUserIndustry")
+//    @ResponseBody
+//    public MessageInfo<UserIndustry> updateUserIndustry (@RequestBody UserIndustry userIndustry){
+//        MessageInfo messageInfo = userIndustryBiz.updateUserIndustry(userIndustry);
+//        return messageInfo;
+//    }
+
+    @RequestMapping("queryIndexHeaderStat")
+    @ResponseBody
+    public MessageInfo<IndexHeaderStat> queryIndexHeaderStat (){
+        MessageInfo messageInfo = indexHeaderStatBiz.getIndexHeaderStat();
+        return messageInfo;
+    }
+}

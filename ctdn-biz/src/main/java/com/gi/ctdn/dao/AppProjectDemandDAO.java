11ed374e86d6045a63b93723b78adefd044d105f
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-11-16 11:48:38 +0800

    dao

---
+++ ctdn-biz/src/main/java/com/gi/ctdn/dao/AppProjectDemandDAO.java
@@ -0,0 +1,12 @@
+package com.gi.ctdn.dao;
+
+import com.gi.ctdn.pojo.AppProjectDemand;
+
+import java.util.List;
+
+public interface AppProjectDemandDAO {
+	
+	int insert(AppProjectDemand appProjectDemand);
+}
+
+

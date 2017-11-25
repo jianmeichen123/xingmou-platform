19714ccde208d4a618354aa64748d3e006a24963
zsl <rezhangsl@163.com>
2017-11-25 17:48:14 +0800

    团队成员添加personcode

--- ctdn-biz/src/main/java/com/gi/ctdn/pojo/ProjectTeam.java
+++ ctdn-biz/src/main/java/com/gi/ctdn/pojo/ProjectTeam.java
@@ -4,120 +4,120 @@ import com.gi.ctdn.view.common.Pagination;
 
 public class ProjectTeam extends Pagination {
 
-	/**
-	 * @Fields projectId : 项目ID
-	 */
-	private String projectCode;
-	
-	/**
-	 * @Fields code : code
-	 */
-	private String code;
-	
-	/**
-	 * @Fields projTitle : 项目名称
-	 */
-	private String projTitle;
-	
-	/**
-	 * @Fields name : 姓名
-	 */
-	private String name;
-	
-	/**
-	 * @Fields job : 职务
-	 */
-	private String job;
-	
-	/**
-	 * @Fields state : 状态
-	 */
-	private String state;
-	
-	/**
-	 * @Fields college : 大学
-	 */
-	private String college;
-	
-	/**
-	 * @Fields edu : 学历
-	 */
-	private String edu;
-	
-	/**
-	 * @Fields introduction : 简介
-	 */
-	private String introduction;
+    /**
+     * @Fields projectId : 项目ID
+     */
+    private String projectCode;
 
-	public void setProjectCode(String projectCode) {
-		this.projectCode = projectCode;
-	}
+    /**
+     * @Fields code : code
+     */
+    private String personCode;
 
-	public String getProjectCode() {
-		return projectCode;
-	}
+    /**
+     * @Fields projTitle : 项目名称
+     */
+    private String projTitle;
 
-	public String getCode(){
-		return code;
-	}
+    /**
+     * @Fields name : 姓名
+     */
+    private String name;
 
-	public void setCode(String code){
-		this.code = code;
-	}
-    
-	public String getProjTitle(){
-		return projTitle;
-	}
+    /**
+     * @Fields job : 职务
+     */
+    private String job;
 
-	public void setProjTitle(String projTitle){
-		this.projTitle = projTitle;
-	}
-    
-	public String getName(){
-		return name;
-	}
+    /**
+     * @Fields state : 状态
+     */
+    private String state;
 
-	public void setName(String name){
-		this.name = name;
-	}
-    
-	public String getJob(){
-		return job;
-	}
+    /**
+     * @Fields college : 大学
+     */
+    private String college;
 
-	public void setJob(String job){
-		this.job = job;
-	}
-    
-	public String getState(){
-		return state;
-	}
+    /**
+     * @Fields edu : 学历
+     */
+    private String edu;
 
-	public void setState(String state){
-		this.state = state;
-	}
-    
-	public String getCollege(){
-		return college;
-	}
+    /**
+     * @Fields introduction : 简介
+     */
+    private String introduction;
 
-	public void setCollege(String college){
-		this.college = college;
-	}
-    
-	public String getEdu(){
-		return edu;
-	}
+    public void setProjectCode(String projectCode) {
+        this.projectCode = projectCode;
+    }
 
-	public void setEdu(String edu){
-		this.edu = edu;
-	}
-    
-	public String getIntroduction(){
-		return introduction;
-	}
+    public String getProjectCode() {
+        return projectCode;
+    }
 
-	public void setIntroduction(String introduction){
-		this.introduction = introduction;
-	}
+    public String getPersonCode() {
+        return personCode;
+    }
+
+    public void setPersonCode(String personCode) {
+        this.personCode = personCode;
+    }
+
+    public String getProjTitle() {
+        return projTitle;
+    }
+
+    public void setProjTitle(String projTitle) {
+        this.projTitle = projTitle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getEdu() {
+        return edu;
+    }
+
+    public void setEdu(String edu) {
+        this.edu = edu;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
 }

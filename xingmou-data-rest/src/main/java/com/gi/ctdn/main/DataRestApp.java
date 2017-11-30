f9ca80d2a5cf13033dd7c078dfce183a5c2aa3c7
vincent <leizhang>
2017-11-30 12:23:32 +0800

    rc监控

--- xingmou-data-rest/src/main/java/com/gi/ctdn/main/DataRestApp.java
+++ xingmou-data-rest/src/main/java/com/gi/ctdn/main/DataRestApp.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by vincent on 16-9-9.
  */
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages={"com.gi.ctdn"})
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})

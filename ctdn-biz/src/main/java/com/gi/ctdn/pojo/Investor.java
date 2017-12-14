9422280524ca7a681c8bcfb64826fbe7e89739b1
zsl <rezhangsl@163.com>
2017-12-14 14:26:15 +0800

    添加创业者及投资人

---
+++ ctdn-biz/src/main/java/com/gi/ctdn/pojo/Investor.java
@@ -0,0 +1,220 @@
+package com.gi.ctdn.pojo;
+
+public class Investor {
+    private Long id;
+    private String code;
+    private String zhName;
+    private String enName;
+    private Integer age;
+    private String gender;
+    private String workLife;
+    private String projCode;
+    private String projTitle;
+    private String introduce;
+    private String job;
+    private String industryIds;
+    private String fields;
+    private String rounds;
+    private Integer districtId;
+    private String districtName;
+    private Integer districtSubId;
+    private String districtSubName;
+    private Integer districtGrandsonId;
+    private String districtGrandsonName;
+    private String colleage;
+    private String degree;
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getZhName() {
+        return zhName;
+    }
+
+    public void setZhName(String zhName) {
+        this.zhName = zhName;
+    }
+
+    public String getEnName() {
+        return enName;
+    }
+
+    public void setEnName(String enName) {
+        this.enName = enName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getWorkLife() {
+        return workLife;
+    }
+
+    public void setWorkLife(String workLife) {
+        this.workLife = workLife;
+    }
+
+    public String getProjCode() {
+        return projCode;
+    }
+
+    public void setProjCode(String projCode) {
+        this.projCode = projCode;
+    }
+
+    public String getProjTitle() {
+        return projTitle;
+    }
+
+    public void setProjTitle(String projTitle) {
+        this.projTitle = projTitle;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getIndustryIds() {
+        return industryIds;
+    }
+
+    public void setIndustryIds(String industryIds) {
+        this.industryIds = industryIds;
+    }
+
+    public String getFields() {
+        return fields;
+    }
+
+    public void setFields(String fields) {
+        this.fields = fields;
+    }
+
+    public String getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(String rounds) {
+        this.rounds = rounds;
+    }
+
+    public Integer getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(Integer districtId) {
+        this.districtId = districtId;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public Integer getDistrictSubId() {
+        return districtSubId;
+    }
+
+    public void setDistrictSubId(Integer districtSubId) {
+        this.districtSubId = districtSubId;
+    }
+
+    public String getDistrictSubName() {
+        return districtSubName;
+    }
+
+    public void setDistrictSubName(String districtSubName) {
+        this.districtSubName = districtSubName;
+    }
+
+    public Integer getDistrictGrandsonId() {
+        return districtGrandsonId;
+    }
+
+    public void setDistrictGrandsonId(Integer districtGrandsonId) {
+        this.districtGrandsonId = districtGrandsonId;
+    }
+
+    public String getDistrictGrandsonName() {
+        return districtGrandsonName;
+    }
+
+    public void setDistrictGrandsonName(String districtGrandsonName) {
+        this.districtGrandsonName = districtGrandsonName;
+    }
+
+    public String getColleage() {
+        return colleage;
+    }
+
+    public void setColleage(String colleage) {
+        this.colleage = colleage;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Investor{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", zhName='" + zhName + '\'' +
+                '}';
+    }
+}
